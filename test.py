b54c1e43d526225f49d2a62fbbbd3fae43397ca2
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-10-25 21:46:00 +0500

    Add comments to test.py for clarity
    Added comments to clarify print statements and for loop.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-print("Hello world")
+print("Hello world")       //it will print the hello
 print("This is a test file for git basics.")
 i
-for i in range(5):
+for i in range(5):    //basic for loop for printing 5 times
   print("I am in Git Basics repository")
