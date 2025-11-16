89e82be11b81be1903c9692150a1f146ada6092f
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-11-16 20:39:06 +0500

    Fix print statement indentation in test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ print("Hello world")       //it will print the hello
 print("This is a test file for git basics.")
 i
 for i in range(5):    //basic for loop for printing 5 times
-  print("I am in Git Basics repository")
+  print("I am in Git Basics repository")    //this is will print
